13621ee35db239491dde64bb349d41a2f03865bd
advik <116435780+advikcreation@users.noreply.github.com>
2023-01-20 22:24:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
-import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { About, Footer, Header, Login, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
+import Home from './Home';
 
 const App = () => (
   <div className="app">
-    <Navbar />
+    <Router >
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+    {/* <Navbar />
     <Header />
     <About />
     <Work />
     <Skills />
     <Testimonial />
-    <Footer />
+    <Footer /> */}
+    </Routes>
+    </Router>
   </div>
 );
 
