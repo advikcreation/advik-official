c386febbcd72d9bc0718c42e551edf35914710cb
advik <116435780+advikcreation@users.noreply.github.com>
2023-01-21 23:02:26 +0530

    Update index.js

--- src/container/index.js
+++ src/container/index.js
@@ -4,6 +4,7 @@ import Header from './Header/Header';
 import Skills from './Skills/Skills';
 import Testimonial from './Testimonial/Testimonial';
 import Work from './Work/Work';
+import Login from './Login/Login';
 
 export {
   About,
@@ -12,4 +13,5 @@ export {
   Skills,
   Testimonial,
   Work,
+  Login,
 };
