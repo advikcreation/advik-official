d2c23b5c4547d00d504e730fafe45cf1939e9965
advik <116435780+advikcreation@users.noreply.github.com>
2023-02-08 12:00:29 +0530

    Update SocialMedia.jsx

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter onClick={() => window.open('https://www.instagram.com/officials_advik/', '_blank')}/>
-    </div>
-    <div>
-      <FaFacebookF onClick={() => window.open('https://www.instagram.com/officials_advik/', '_blank')}/>
-    </div>
-    <div>
       <BsInstagram onClick={() => window.open('https://www.instagram.com/officials_advik/', '_blank')}/>
     </div>
+    <div>
+      <FaLinkedin onClick={() => window.open('https://www.linkedin.com/in/advikofficial', '_blank')}/>
+    </div>
+    <div>
+      <BsTwitter onClick={() => window.open('https://www.instagram.com/officials_advik/', '_blank')}/>
+    </div>
+    
   </div>
 );
 
